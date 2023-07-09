e4c934b35a97387233abb588141f13ddfc0ed279
张海盐 <75735147+Baysalt-Zhang@users.noreply.github.com>
2023-07-09 12:57:29 +0800

    lab3_challenge2_semaphore

---
+++ user/user_lib.h
@@ -0,0 +1,14 @@
+/*
+ * header file to be used by applications.
+ */
+
+int printu(const char *s, ...);
+int exit(int code);
+void* naive_malloc();
+void naive_free(void* va);
+int fork();
+void yield();
+//added @lab3_challenge2
+int sem_new(int);
+int sem_P(int);
+int sem_V(int);
